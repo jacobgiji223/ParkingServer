158917aceb5ce659adb97bd7d3b0cfaa5c2abb32
jacobgiji223 <jacobgiji223@gmail.com>
2026-03-12 14:22:00 +0530

    Enhance grid slots with IDs and Firebase integration
    Added IDs to grid slots and implemented Firebase data fetching to update slot backgrounds based on availability.

--- script.js
+++ script.js
@@ -8,9 +8,36 @@ const totalColumns = 15;
 
 for (let r = 0; r < rows.length; r++) {
   for (let c = 1; c <= totalColumns; c++) {
+
+    const id = rows[r] + c;
+
     const div = document.createElement("div");
     div.className = "slot";
-    div.textContent = rows[r] + c;  // A1, A2 ... Z15
+    div.textContent = id;
+    div.id = id;
+
     grid.appendChild(div);
   }
 }
+
+/* READ DATA FROM FIREBASE */
+
+setInterval(() => {
+
+fetch("https://parkingserver-964ae-default-rtdb.asia-southeast1.firebasedatabase.app/parking.json")
+.then(res => res.json())
+.then(data => {
+
+  const A1 = document.getElementById("A1");
+  const A4 = document.getElementById("A4");
+  const A5 = document.getElementById("A5");
+  const A6 = document.getElementById("A6");
+
+  if(data.A1 == 1) A1.style.background="#dc2626"; else A1.style.background="#16a34a";
+  if(data.A4 == 1) A4.style.background="#dc2626"; else A4.style.background="#16a34a";
+  if(data.A5 == 1) A5.style.background="#dc2626"; else A5.style.background="#16a34a";
+  if(data.A6 == 1) A6.style.background="#dc2626"; else A6.style.background="#16a34a";
+
+});
+
+},1000);
